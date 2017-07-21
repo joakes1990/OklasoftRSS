1b833ec833729ca98ef7d67fc4ad5deddbd1899e
Justin Oakes <justin@justins-macbook-pro.local>
2017-07-21 17:03:12 -0500

    Discovered the magic RegEx committing before it's lost for good.

---
+++ OklasoftRSS/Parsers/HTMLtoString.swift
@@ -0,0 +1,16 @@
+//
+//  HTMLtoString.swift
+//  OklasoftRSS
+//
+//  Created by Justin Oakes on 7/21/17.
+//  Copyright © 2017 Oklasoft LLC. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    
+    func getPlainTextFromHTML() {
+        let regEx: String = "/(<.*?.>)/g"
+    }
+}
