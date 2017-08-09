0de708ce8d79768e7ec510430aba34a77733ebaf
Justin Oakes <admin@oklasoftware.com>
2017-08-09 13:56:15 -0500

    removed unused observer class

--- OklasoftRSS/Notifications.swift
+++ OklasoftRSS/Notifications.swift
@@ -12,13 +12,10 @@ public let feedInfoKey = "feed"
 public let storyInfoKey = "story"
 public extension Notification.Name {
     static let finishedReceavingFeed = Notification.Name("finishedReceavingFeed")
-//    static let finishedReceavingRSSStory = Notification.Name("finishedReceavingRSSData")
-//    static let finishedReceavingAtomStory = Notification.Name("finishedReceavingRSSAtomData")
-//    static let finishedReceavingJSONStory = Notification.Name("finishedReceavingRSSJSONFeedData")
     static let foundFeedURLs = Notification.Name("foundFeedURLs")
-    static let feedIdentificationError = Notification.Name("notificationError")
     static let foundFavIcon = Notification.Name("foundFavIcon")
     static let finishedFindingStories = Notification.Name("finishedFindingStories")
+    static let feedIdentificationError = Notification.Name("notificationError")
     static let errorFindingStories = Notification.Name("errorFindingStories")
     static let errorConvertingHTML = Notification.Name("errorConvertingHTML")
 }
